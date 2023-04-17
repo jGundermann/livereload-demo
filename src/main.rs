7397c88781e479d80a058e183dcc3c05780b066e
jGundermann <joris.gundermann@toucantoco.com>
2023-04-17 20:53:39 +0200

    add home route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(home_handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn home_handler() -> String {
+    "Hello world".to_string()
 }
